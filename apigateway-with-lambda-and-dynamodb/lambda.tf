71061a533cfdba1672809cebd1acce0fe2e7a2df
vak-rashu <rashu.cloud11@gmail.com>
2025-05-30 18:19:46 +0530

    Delete apigateway-with-lambda-and-dynamodb directory

--- apigateway-with-lambda-and-dynamodb/lambda.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_lambda_function" "api_lambda" {
-    function_name = "lambda-api"
-    filename = "function.zip"
-    runtime = "python3.12"
-    role = "arn:aws:iam::000000000000:role/lamabda-role"
-    handler = "lambda.handler"
-}
